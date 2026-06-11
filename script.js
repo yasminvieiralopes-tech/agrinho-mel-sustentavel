0419bd71d092a0a02e5f5724e97de26c368c4535
yasminvieiralopes-tech <yasmin.vieira.lopes@escola.pr.gov.br>
2026-06-11 09:38:02 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Aguarda o carregamento do HTML
+document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.getElementById("btn-acao");
+
+    // Adiciona um evento de clique ao botão
+    botao.addEventListener("click", () => {
+        alert("Obrigado por se interessar pelo Mel Sustentável! Juntos protegemos a natureza. 🐝");
+    });
+});
